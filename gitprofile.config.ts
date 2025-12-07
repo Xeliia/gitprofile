fb557bb22f5244f2dca18791849d48631ebc313d
Xeliia <140964793+Xeliia@users.noreply.github.com>
2025-12-07 22:57:30 +0800

    Increase project display limit and update exclusions

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -17,10 +17,10 @@ const CONFIG = {
       mode: 'automatic', // Mode can be: 'automatic' or 'manual'
       automatic: {
         sortBy: 'updated', // Sort projects by 'stars' or 'updated'
-        limit: 8, // How many projects to display.
+        limit: 20, // How many projects to display.
         exclude: {
           forks: false, // Forked projects will not be displayed if set to true.
-          projects: [], // These projects will not be displayed. example: ['arifszn/my-project1', 'arifszn/my-project2']
+          projects: ['Xeliia/gitprofile', 'Xeliia/Odin-Recipes', 'Xeliia/kickstart.nvim', 'Xeliia/Bento', 'Xeliia/DSA_Project-Bank', 'Xeliia/Homepage', 'Xeliia/end4-modified', 'Xeliia/devops-project', 'Xeliia/TaskTide'], // These projects will not be displayed. example: ['arifszn/my-project1', 'arifszn/my-project2']
         },
       },
       manual: {
